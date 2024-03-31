c7b23384ffd0d9d445daa68f6b4cdd13bd6594c4
alchemist1111 <emoyoeugine@gmail.com>
2024-03-31 22:08:54 +0300

    addsindex.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+       console.log('I was clicked!');
+    });
 }
+addEventListener('I was clicked!');
